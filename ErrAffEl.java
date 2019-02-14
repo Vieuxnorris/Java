6ab7270e63ebac3a5ed2b8b1039baf8699ba4615
Vieuxnorris <vieuxnorris@gmail.com>
2019-02-14 23:30:22 +0100

    Update ErrAffEl.java

--- ErrAffEl.java
+++ ErrAffEl.java
@@ -1,10 +1,13 @@
+// programme qui montre une convertion forcé
+// fait par **Julien Wins** et **Gordon Freeman**
+// Fait le 14/02/2019
 public class ErrAffEl
 {
     public static void main(String args[])
     {
         byte b = 10;
         int n = 10000;
-        b += n;
+        b += n; // comparable a un "b = (byte) (b+n)"
         System.out.println("b = " + b);
     }
 }
